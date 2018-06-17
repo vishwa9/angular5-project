00d3516720568e5eb7bca87fa4a19950180af443
vishwajeet singh chauhan <vishwajeet11104736@gmail.com>
2018-06-17 12:18:43 +0530

    binding concept added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,32 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers', //use as attribute '[app-servers]' or '.app-servers'
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!!';
+  serverName = 'test';
+  serverCreated = false;
+  servers = ['Testserver','Testserver 2'];
+  constructor() {
+      setTimeout(() => {
+        this.allowNewServer = true;
+      },2000);
+   }
 
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is '+ this.serverName;
+  }
+  onUpdateServerName(event:Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
